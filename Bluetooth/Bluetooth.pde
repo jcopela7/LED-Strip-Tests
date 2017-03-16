f4a1734b3fe8324d122cb04c581f899e2f63ef91
jcopela7 <joncope8@gmail.com>
2017-03-15 20:07:07 -0400

    Added rainbow mode and cleaned arduino code

--- Bluetooth/Bluetooth.pde
+++ Bluetooth/Bluetooth.pde
@@ -3,7 +3,11 @@ import processing.serial.*;
 Serial myPort;
 String ledStatus="LED: Off";
 int status= 0;
-int hue=2;
+int hue=3;
+
+int r,g,b,r1,g1,b1,r2,g2,b2;
+
+int mode=0;
 
 
 
@@ -25,11 +29,20 @@ void draw() {
    strokeWeight(1);
    rect(50,100,150,50,10);
    rect(250,100,150,50,10);
+   fill(r,g,b);
+   rect(20,10,130,50,10);
+   fill(r1,g1,b1);
+   rect(160,10,130,50,10);
+   fill(r2,g2,b2);
+   rect(300,10,130,50,10);
    fill(255);
    
    textSize(32);
    text("ON", 60,135);
    text("OFF",255,135);
+   text("Mode1",30,50);
+   text("Mode2",170,50);
+   text("Mode3",310,50);
    fill(0);
    
    text(ledStatus, 155,240);
@@ -40,12 +53,61 @@ void draw() {
    rect(75,325,50,50);
    
    colorMode(HSB,hue,187,200);
-   fill(255,255,255);
+   fill(100,100,100);
    ellipse(225,350,150,150);
    colorMode(RGB,255,255,255,255);
    
-   if(mousePressed && mouseX>50 && mouseX<200 && mouseY>100 && mouseY<150){
+   if(mousePressed && mouseX>20 && mouseX<170 && mouseY>10 && mouseY<60){
+     r=234;
+     g=100;
+     b=30;
+     r1=20;
+     g1=160;
+     b1=133;
+     r2=20;
+     g2=160;
+     b2=133;
+     mode=1; 
+   }
+   
+   if(mousePressed && mouseX>160 && mouseX<290 && mouseY>10 && mouseY<60){
+     r=20;
+     g=160;
+     b=133;
+     r1=234;
+     g1=100;
+     b1=30;
+     r2=20;
+     g2=160;
+     b2=133;
+     mode=2; 
+   }
+   
+   if(mousePressed && mouseX>300 && mouseX<430 && mouseY>10 && mouseY<60){
+     r=20;
+     g=160;
+     b=133;
+     r1=20;
+     g1=160;
+     b1=133;
+     r2=234;
+     g2=100;
+     b2=20;
+     mode=3; 
+   }
+   
+   
+   if(mousePressed && mouseX>50 && mouseX<200 && mouseY>100 && mouseY<150 && mode==1){
      status=1;
+     myPort.write(status);
+     stroke(255,0,0);
+     strokeWeight(2);
+     noFill();
+     rect(50,100,150,50,10);
+   }
+   
+   if(mousePressed && mouseX>50 && mouseX<200 && mouseY>100 && mouseY<150 && mode==2){
+     status=2;
      myPort.write(status);
      stroke(255,0,0);
      strokeWeight(2);
@@ -71,7 +133,7 @@ void draw() {
    } 
    
    if(mousePressed && mouseX>75 && mouseX<125 && mouseY>325 && mouseY<375){
-    if(hue>2){
+    if(hue>3){
      hue--;
      myPort.write(hue);
     }
--- HSV_Colour_Change/HSV_Colour_Change.ino
+++ HSV_Colour_Change/HSV_Colour_Change.ino
@@ -8,12 +8,14 @@ int state=0;
 int bluetooth_Data=0;
 
 //Colour Variables
+int rainbow=1;
 int hue=1;
 int hroc=1;//hue rate of change
 
 //Timing Variables
 const long interval=50;
 long previousMillis=0;
+long previousMillis2=0;
 
 
 CRGB leds[NUM_LEDS];
@@ -27,41 +29,91 @@ void setup(){
 
 void loop(){
 
-    bluetooth_Data=Serial.read();
+    //Collect Data via SPI communication 
+    bluetooth_Data=Serial.read();//read incoming data
+    
+    // Turn Strip On
     if(bluetooth_Data==1){
-    state= 1;}
+    state= 1;
+    }
+
+    //Turn Strip Off
     if(bluetooth_Data==0){
-    state=0;}
-    if(bluetooth_Data>=2){
+    state=0;
+    }
+
+    //Rainbow Mode
+    if(bluetooth_Data==2){
+    state=2;
+    }
+
+    //Manual Hue Change
+    if(bluetooth_Data>=3 && bluetooth_Data<=256){
       hue=bluetooth_Data;
     }
+
+
   
   unsigned long currentMillis= millis();
   
   if(currentMillis - previousMillis >= interval && state==1){
-  previousMillis=currentMillis;
-  for (int i=0; i<NUM_LEDS; i++){
-  leds[i]=CHSV(hue,187,200); //Using hue, saturation, and brightness to set LED colour
-  }
-  /*hue+=hroc;
-  
-  if(hroc==244){
-    hroc=-1;
-  }
-  if(hroc==0){
-    hroc=1;
-  }*/
-  FastLED.show();
-  Serial.println("LED: On");
+    previousMillis=currentMillis;
+    manualHueChange(hue);
   }
 
   if(state==0){
-    for (int i=0; i<NUM_LEDS; i++){
-    leds[i]=CRGB(0,0,0);
-    }
-  FastLED.show();
-  Serial.println("LED: Off");
+    LED_Off();
+  }
+ 
+  unsigned long currentMillis2=millis();
+
+  if(currentMillis2 - previousMillis2 >= interval && state==2){
+    previousMillis2=currentMillis2;
+    rainbowAutoChange();
   }
 
+  FastLED.show();
 }
+
+void LED_Off(){
+  
+  for (int i=0; i<NUM_LEDS; i++){
+    leds[i]=CRGB(0,0,0);
+  }
+
+  Serial.println("LED: Off");  
+}
+
+void manualHueChange (int hue){
+  
+  for (int i=0; i<NUM_LEDS; i++){
+  leds[i]=CHSV(hue,187,200); //Using hue, saturation, and brightness to set LED colour
+  }
+
+  Serial.println("LED: On");  
+}
+
+
+void rainbowAutoChange(){
+  for (int i=0; i<NUM_LEDS; i++){
+  leds[i]=CHSV(rainbow,187,200); //Using hue, saturation, and brightness to set LED colour
+  }
+
+  rainbow+=hroc;
+  
+  if(rainbow==244){
+    hroc=-1;
+  }
+  if(rainbow==0){
+    hroc=1;
+  }
+  
+  FastLED.show();
+
+  Serial.println("Rainbow Mode");
+  
+}
+
+
+
 
